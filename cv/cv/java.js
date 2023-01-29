984587f773950d5f32f97bd74253c7511b7307b6
FrancoEzequielDeLuca <123904129+FrancoEzequielDeLuca@users.noreply.github.com>
2023-01-29 18:26:38 -0300

    Add files via upload

---
+++ cv/cv/java.js
@@ -0,0 +1,11 @@
+
+
+
+//document.getElementById('presentacion').onclick=function () {
+//console.log("hola")
+//document.getElementById("demo").innerHTML = "Presentación" +"Considero ser una persona responsable y comprometido con mi tarea. Mi principal cualidad radica en disfrutar de mi trabajo, lo cual me impulsa a querer mejorar cada día. Mis superioes siempre me han apreciado por ser una persona respetuosa de las normas, solidaria y que mantiene una buena relación con sus colegas." }
+
+
+
+
+
